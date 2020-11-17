323833bdb73a6c7022878ad8545eb03dd74e7ea2
Jueun Jeon <jjeon02@risd.edu>
2020-11-17 13:28:48 +0900

    erase_visual

--- visual/index.js
+++
@@ -1,8 +0,0 @@
-function navDrop() {
-    var x = document.getElementById("myNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  }
