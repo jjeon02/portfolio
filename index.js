ef200961e00628fc9afe3f3ff30ee0cc63dc248f
Jueun Jeon <june.jeon@gmail.com>
2021-06-03 16:27:06 +0900

    illust update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
 //   }
 // }
 
+//NAVIGATION
 function openNav() {
   document.getElementById("sideNav").style.width= "100%";
 }
@@ -22,6 +23,7 @@ function closeNav() {
   document.getElementById("sideNav").style.width = "0";
 }
    
+// SLIDE
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -35,16 +37,11 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slides");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
